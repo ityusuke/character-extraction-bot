631aeee09eddf7fe53e3d2ccb132ffd0de07b088
yusuke katayama <yusuke.katayama@homie.co.jp>
2022-02-05 21:29:33 +0900

    LINEメッセージで送信された画像を解析して結果を返却するように

--- go.mod
+++ go.mod
@@ -1,5 +1,15 @@
 module reprinting-check-bot
 
+// +heroku goVersion go1.15
 go 1.16
 
-require cloud.google.com/go/vision v1.0.0
+require (
+	cloud.google.com/go/vision v1.2.0
+	github.com/gin-gonic/gin v1.8.1
+	github.com/line/line-bot-sdk-go/v7 v7.16.0
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	golang.org/x/crypto v0.0.0-20220131195533-30dcbda58838 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd
+	golang.org/x/oauth2 v0.0.0-20211104180415-d3ed0bb246c8
+	google.golang.org/api v0.70.0
+)
--- main.go
+++ main.go
@@ -2,48 +2,154 @@ package main
 
 import (
 	vision "cloud.google.com/go/vision/apiv1"
-	"context"
+	"errors"
 	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/line/line-bot-sdk-go/v7/linebot"
+	"golang.org/x/net/context"
+	"golang.org/x/oauth2/google"
+	"google.golang.org/api/option"
+	"io"
 	"log"
 	"os"
 )
 
+type (
+	detectionWebPage struct {
+		url       string `protobuf:"bytes,1,opt,name=url,proto3" json:"url,omitempty"`
+		pageTitle string `protobuf:"bytes,3,opt,name=page_title,json=pageTitle,proto3" json:"page_title,omitempty"`
+	}
+)
+
 const (
-	sampleImagePath = "./image/sample.jpeg"
+	lineMessageAPIChannelSecretKey = "LINE_BOT_CHANNEL_SECRET"
+	lineMessageAPIChannelTokenKey  = "LINE_BOT_CHANNEL_TOKEN"
+	OpenVisionAPICredential        = "GOOGLE_APPLICATION_CREDENTIALS"
+	lineMessageAPICallBackEndpoint = "/callback"
 )
 
 func main() {
-	ocr(sampleImagePath)
+	log.Println("gen bis cli")
+	jwtConfig, err := google.JWTConfigFromJSON([]byte(os.Getenv(OpenVisionAPICredential)), vision.DefaultAuthScopes()...)
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx := context.Background()
+	visClient, err := vision.NewImageAnnotatorClient(ctx, option.WithTokenSource(jwtConfig.TokenSource(ctx)))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("gen line cli")
+	lineClient, err := linebot.New(os.Getenv(lineMessageAPIChannelSecretKey), os.Getenv(lineMessageAPIChannelTokenKey))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	engine := gin.Default()
+	engine.GET("/ping", func(c *gin.Context) {
+		log.Println("pong")
+	})
+	engine.POST(lineMessageAPICallBackEndpoint, func(c *gin.Context) {
+		err = exec(lineClient, visClient, ctx, c)
+		if err != nil {
+			log.Println(err)
+		}
+	})
+	engine.Run(":" + os.Getenv("PORT"))
 }
 
-func ocr(fileName string) {
-	ctx := context.Background()
-
-	client, err := vision.NewImageAnnotatorClient(ctx)
+func exec(lineClient *linebot.Client, visClient *vision.ImageAnnotatorClient, ctx context.Context, c *gin.Context) error {
+	log.Println("start exec")
+	events, err := lineClient.ParseRequest(c.Request)
 	if err != nil {
-		log.Fatalf("Failed to create client: %v", err)
+		return err
 	}
 
-	file, err := os.Open(fileName)
+	log.Println("start extractImageFromLINEMessage")
+	file, replyToken, err := extractImageFromLINEMessage(lineClient, events)
 	if err != nil {
-		log.Fatalf("Failed to read file: %v", err)
+		return err
 	}
-	defer file.Close()
+
+	log.Println("start checkReprint")
+	detectionWebPages, err := checkReprint(file, visClient, ctx)
+	if err != nil {
+		return err
+	}
+
+	log.Println("start sendLINEMessageWithMatchWebPages")
+	return sendLINEMessageWithMatchWebPages(lineClient, replyToken, detectionWebPages)
+}
+
+func extractImageFromLINEMessage(lineClient *linebot.Client, events []*linebot.Event) (*os.File, string, error) {
+	for _, event := range events {
+		if event.Type == linebot.EventTypeMessage {
+			switch message := event.Message.(type) {
+			case *linebot.ImageMessage:
+				file, err := os.Create("sample.png")
+				if err != nil {
+					return nil, "", err
+				}
+
+				log.Println("GetMessageContent")
+				content, err := lineClient.GetMessageContent(message.ID).Do()
+				if err != nil {
+					return nil, "", err
+				}
+				defer content.Content.Close()
+				io.Copy(file, content.Content)
+				return file, event.ReplyToken, nil
+			default:
+				if _, err := lineClient.ReplyMessage(event.ReplyToken, linebot.NewTextMessage("画像を送信してください")).Do(); err != nil {
+					return nil, "", err
+				}
+			}
+		}
+	}
+	return nil, "", errors.New("failed extract image from line message")
+}
+
+func checkReprint(file *os.File, visClient *vision.ImageAnnotatorClient, ctx context.Context) ([]*detectionWebPage, error) {
+	detectionWebPages := make([]*detectionWebPage, 0)
+
 	image, err := vision.NewImageFromReader(file)
+	defer file.Close()
 	if err != nil {
-		log.Fatalf("Failed to create image: %v", err)
+		log.Println(err)
+		return detectionWebPages, err
 	}
 
-	detection, err := client.DetectWeb(ctx, image, nil)
+	detection, err := visClient.DetectWeb(ctx, image, nil)
 	if err != nil {
-		log.Fatalf("Failed to detect labels: %v", err)
+		return detectionWebPages, err
 	}
+
 	matchImages := detection.GetPagesWithMatchingImages()
-	if len(matchImages) == 0 {
-		log.Println("failed to find image in web")
-	}
 	for _, matchImage := range matchImages {
-		fmt.Println(matchImage.PageTitle)
-		fmt.Println(matchImage.Url)
+		detectionWebPages = append(detectionWebPages, &detectionWebPage{matchImage.Url, matchImage.PageTitle})
 	}
+
+	return detectionWebPages, nil
+}
+
+func sendLINEMessageWithMatchWebPages(lineClient *linebot.Client, replyToken string, detectionWebPages []*detectionWebPage) error {
+	if len(detectionWebPages) == 0 {
+		if _, err := lineClient.ReplyMessage(replyToken, linebot.NewTextMessage("拾い画ではありません")).Do(); err != nil {
+			return err
+		}
+	} else {
+		var message string
+		for i, detectionWebPage := range detectionWebPages {
+			message += fmt.Sprintf("WEBページ名: %s\n", detectionWebPage.pageTitle)
+			message += fmt.Sprintf("URL: %s\n", detectionWebPage.url)
+			if i < len(detectionWebPages) {
+				message += fmt.Sprintln("\n")
+			}
+		}
+		if _, err := lineClient.ReplyMessage(replyToken, linebot.NewTextMessage(message)).Do(); err != nil {
+			return err
+		}
+	}
+	return errors.New("failed to send line message")
 }
